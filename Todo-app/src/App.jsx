7cef51c9fbeeaa9c32c18973f7afd67b24bc8fe8
Endy1381 <119288346+Endy1381@users.noreply.github.com>
2023-09-15 10:08:12 -0700

    Add todo
    added the basic todo functionality.

--- Todo-app/src/App.jsx
+++ Todo-app/src/App.jsx
@@ -1,16 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([
+    { id: 1, text: 'learn React', completed: true },
+    { id: 2, text: 'it works now!', completed: false }
+  ]);
+  const input = useRef();
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newTodo = {
+      id: todos.length + 1,
+      text: input.current.value,
+      completed: false
+    };
+    setTodos([...todos, newTodo]);
+    input.current.value = '';
+  }
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id));
+  }
   return (
     <>
-      <h1 className='text-center'>Hello world</h1>
+      <h1 className='text-center'>Todo app</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          ref={input}
+          className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
+        />
+        <button
+          type="submit"
+          className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+        >
+          add
+        </button>
+      </form>
+      <ul>
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>{todo.text}  <button onClick={() => removeTodo(todo.id)} className='ml-5 bg-blue-500 text-white font-bold py-2 px-2 rounded'>
+            remove
+          </button></li>
+          );
+        })}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
